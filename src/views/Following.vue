94572b695bd296178feb0833f2a58beff871c57d
zhaoyun1996 <kiet14020248uet@gmail.com>
2022-05-08 09:25:03 +0700

    Thêm page

---
+++ src/views/Following.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="following">
+        <h2>Following page</h2>
+    </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="home">
+        <h2>Home page</h2>
+    </div>
+</template>
